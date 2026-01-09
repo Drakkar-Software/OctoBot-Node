ed1cad71799c76920d4b67a69f5788791741ad2d
Guillaume De Saint Martin <guillaumemdsm@gmail.com>
2026-01-09 13:48:14 +0100

    [Sentry] avoid uploading local variable

--- octobot_node/app/main.py
+++ octobot_node/app/main.py
@@ -38,7 +38,11 @@ def custom_generate_unique_id(route: APIRoute) -> str:
 
 
 if settings.SENTRY_DSN and settings.ENVIRONMENT != "local":
-    sentry_sdk.init(dsn=str(settings.SENTRY_DSN), enable_tracing=True)
+    sentry_sdk.init(
+        dsn=str(settings.SENTRY_DSN),
+        enable_tracing=True,
+        include_local_variables=False,   # careful not to upload sensitive data
+    )
 
 
 @asynccontextmanager
